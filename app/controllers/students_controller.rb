f28ea670a209c4f52fb64746f2f011836e070d14
Fred Higgins <higgins.fred.s@gmail.com>
2022-05-26 16:53:00 -0700

    commit tree 41c92f5c0ad9f8ba3645ec95f497248eda5363d4

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        students = Student.all.order(:grade).reverse
+        render json: students
+    end
+
+    def highest_grade
+        student = Student.all.order(:grade).reverse.first
+        render json: student
+    end
+
+end
